61db91742a0f21206ebd2f5ed4b82a5730e9561f
CodingWithBars <barrojohnnems01@gmail.com>
2025-06-18 09:26:36 +0800

    e
    e

--- main.py
+++ main.py
@@ -21,8 +21,10 @@ from geopy.geocoders import Nominatim
 from fastapi.encoders import jsonable_encoder
 from fastapi import Query
 from typing import Optional
-import tensorflow as tf
-Interpreter = tf.lite.Interpreter
+try:
+    from tflite_runtime.interpreter import Interpreter
+except ImportError:
+    from tensorflow.lite.python.interpreter import Interpreter
 
 # Ensure these are already defined
 from config import ALLOWED_EXTENSIONS, MAX_UPLOAD_SIZE, UPLOAD_DIR, API_BASE_URL
